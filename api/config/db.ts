9f3d5f2bdae709bc4dddaa13d7be0800064ad3a4
fayaz <fayaz@gravityilabs.com>
2021-11-02 23:47:58 +0530

    connected to mongo db

--- api/config/db.ts
+++ api/config/db.ts
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
     try{
-        const conn = await mongoose.connect()
+        const conn = await mongoose.connect(process.env.MONGO_URI!);
+        console.log("DB Connected!!!");
+        return conn;
     }catch(error: any){
         process.exit(1);
     }
 }
+
+export default connect;
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connect from '../config/db';
 
 dotenv.config({
     path: './config/config.env'
@@ -7,5 +8,5 @@ dotenv.config({
 
 const app = express();
 const port = process.env.PORT;
-
+connect();
 app.listen(port, () => console.log(`running on ${process.env.DEV_ENV} mode and on server ${process.env.PORT}`));
